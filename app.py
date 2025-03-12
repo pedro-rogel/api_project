5060b2b66f621beb17ca4e09a98b5669ff3529a8
Pedro Rogel <pedrorogel3@gmail.com>
2025-03-12 20:11:55 -0300

    feat: add flask no projeto

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Testando o repos")
+from flask import Flask, jsonify
+app = Flask(__name__)
+app.route('https://viacep.com.br/ws/01001000/json/', methods=['GET'])
+def ex():
+    dados = {'message': 'Welcome'}
+    return jsonify(dados)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
