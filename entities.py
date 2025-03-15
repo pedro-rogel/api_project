95e78f87eeec549fcd282a2ff94cd68c70fa0336
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-15 09:52:43 -0300

    refactor: rename students.py to entities.py and update its content

---
+++ entities.py
@@ -0,0 +1,24 @@
+api_entidades =  {
+    "students": [
+    {
+        "id": 1,
+        "name": "Felippe", 
+    },
+    {
+        "id": 2,
+        "name": "Fernando",
+    },
+    {
+        "id": 3,
+        "name": "Gustavo",
+    },
+    {
+        "id": 4,
+        "name": "Murillo",
+    },
+    {
+        "id": 5,
+        "name": "Pedro",
+    }
+    ]
+}
