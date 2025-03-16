b429d901a1fc5ba3252bbe6cf7073d5c39302c55
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-16 08:09:24 -0300

    refactor: improve the entities model structure in api/entities.py

--- apis/entities.py
+++ apis/entities.py
@@ -1,50 +1,36 @@
 api_entidades =  {
     "alunos": [
-    {
-        "id": 1000,
-        "nome": "Felippe", 
-    },
-    {
-        "id": 2000,
-        "nome": "Fernando",
-    },
-    {
-        "id": 3000,
-        "nome": "Gustavo",
-    },
-    {
-        "id": 4000,
-        "nome": "Murillo",
-    },
-    {
-        "id": 5000,
-        "nome": "Pedro",
-    }],
+        {
+            "id": 100,
+            "nome": "Felipe Santos",
+            "idade": 22, 
+            "data_nascimento": "2003/02/15", 
+            "nota_primeiro_semestre": 10.0, 
+            "nota_segundo_semestre": 10.0, 
+            "media_final": 10.0,
+            "turma_id": 100, 
+        }
+    ],
 
 
-    "professores": [
-    {
-        "id": 1000,
-        "nome": "João", 
-    },
-    {
-        "id": 2000,
-        "nome": "Mário",
-    },
-    {
-        "id": 3000,
-        "nome": "Antônio",
-    }],
+        "professores": [
+        {
+            "id": 100,
+            "nome": "João Augusto",
+            "idade": 49,
+            "data_nascimento": "1975/10/20",
+            "disciplina": "Desenvolvimento Web",
+            "salario": 5000.0
+        }
+    ],
 
 
-    "turmas": [
-    {
-        "id": 1000,
-        "nome": "ADS", 
-    },
-    {
-        "id": 2000,
-        "nome": "SI",
-    },
+        "turmas": [
+        {
+            "id": 100,
+            "nome": "ADS",
+            "turno": "Noite",
+            "professor_id": 100
+        }
     ]
 }
