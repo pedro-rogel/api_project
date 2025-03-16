dff5f61deda47a2a8c1919a0e9f2d3b44201f250
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-16 08:13:11 -0300

    feat: add blueprints for modular API structure and refactor main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from routes.alunos import alunos_bp
+from routes.professores import professores_bp
+from routes.turmas import turmas_bp
 
 app = Flask(__name__)
 
-from routes import *
+app.register_blueprint(alunos_bp)
+app.register_blueprint(professores_bp)
+app.register_blueprint(turmas_bp)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
