00877c7b8268091c7f6b8d2787fc1e44289e5481
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-16 09:06:20 -0300

    feat: add /reseta route to clear all entities

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask
-from routes.alunos import alunos_bp
-from routes.professores import professores_bp
-from routes.turmas import turmas_bp
+from flask import Flask, jsonify
+from routes.alunos import alunos_bp, alunos
+from routes.professores import professores_bp, professores
+from routes.turmas import turmas_bp, turmas
 
 app = Flask(__name__)
 
 app.register_blueprint(alunos_bp)
 app.register_blueprint(professores_bp)
 app.register_blueprint(turmas_bp)
+
+@app.route("/reseta", methods=['POST'])
+def reset_server():
+    alunos.clear()
+    professores.clear()
+    turmas.clear()
+    return jsonify(message="resetado com sucesso")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
