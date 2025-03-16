76861521710280a012cda151e9212729925429b1
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-16 00:00:22 -0300

    feat: multiple updates in routes.py

--- routes.py
+++ routes.py
@@ -1,41 +1,46 @@
-from flask import jsonify, make_response, request
+from flask import jsonify, request
 from main import app
 from apis.entities import api_entidades
 
-app.config['JSON_SORT_KEYS'] = False
+alunos = api_entidades["alunos"]
 
 @app.route("/alunos", methods=['GET'])
 def get_student():
-    if not api_entidades["students"]:
-        return make_response(jsonify(message="No registered students"))
-    return make_response(jsonify(data=api_entidades["students"], message="List of students"))
+    return jsonify(alunos)
 
 @app.route(f"/alunos/<int:id>", methods=['GET'])
 def get_student_id(id):
-    for student in api_entidades["students"]:
-        if student["id"] == id:
-            return jsonify(student)
-    return jsonify({"error": "Student not found"}), 404
+    for aluno in alunos:
+        if aluno["id"] == id:
+            return jsonify(aluno)
+    return jsonify({"erro":"aluno nao encontrado"}), 404
 
 @app.route("/alunos", methods=['POST'])
 def create_student():
-    student = request.json
-    api_entidades["students"].append(student)
-    return make_response(jsonify(data=student, message="Successful creation"))
+    novo_aluno = request.json
+    if not any(aluno["id"] == novo_aluno["id"] for aluno in alunos):
+        alunos.append(novo_aluno)
+        return jsonify(message="criado com sucesso")
+    return jsonify({"erro":"id ja utilizada"}), 400
 
 @app.route("/alunos/<int:id>", methods=['PUT'])
 def update_student(id):
-    for student in api_entidades["students"]:
-        if student["id"] == id:
-            r = request.json
-            student["name"] = r["name"]
-            return make_response(jsonify(message="Successful update"))
-    return jsonify({"error": "Student not found"}), 404
+    for aluno in alunos:
+        if aluno["id"] == id:
+            atualizar_nome = request.json
+            aluno["nome"] = atualizar_nome["nome"]
+            return jsonify(message="atualizado com sucesso")
+    return jsonify({"erro":"aluno nao encontrado"}), 404
 
 @app.route("/alunos/<int:id>", methods=['DELETE'])
 def delete_student(id):
-    for student in api_entidades["students"]:
-        if student["id"] == id:
-            api_entidades["students"].remove(student)
-            return make_response(jsonify(message="Successful deletion"))
-    return jsonify({"error": "Student not found"}), 404
+    for aluno in alunos:
+        if aluno["id"] == id:
+            alunos.remove(aluno)
+            return jsonify(message="deletado com sucesso")
+    return jsonify({"erro":"aluno nao encontrado"}), 404
+
+@app.route("/reseta", methods=['POST'])
+def reset_server():
+    alunos.clear()
+    return jsonify(message="resetado com sucesso")
