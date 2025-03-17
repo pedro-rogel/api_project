308dd699717cb5a2fa4bf8873e0d7cab2a5be239
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-17 20:33:54 -0300

    refactor: split entities_test.py to entities.py

--- apis/entities.py
+++ apis/entities.py
@@ -1,34 +1,47 @@
-api_entidades =  {
-    "alunos": [
-        {
-            "id": 100,
-            "nome": "Felipe Santos",
-            "idade": 22, 
-            "data_nascimento": "2003/02/15", 
-            "nota_primeiro_semestre": 10.0, 
-            "nota_segundo_semestre": 10.0, 
-            "media_final": 10.0,
-            "turma_id": 100, 
-        }
-    ],
+import json
+from entities_test import *
 
-    "professores": [
-        {
-            "id": 100,
-            "nome": "João Augusto",
-            "idade": 49,
-            "data_nascimento": "1975/10/20",
-            "disciplina": "Desenvolvimento Web",
-            "salario": 5000.0
-        }
-    ],
+api_entidades = {
+    "Alunos": [
 
-    "turmas": [
-        {
-            "id": 100,
-            "nome": "ADS",
-            "turno": "Noite",
-            "professor_id": 100
-        }
+    ],
+    "Professores": [
+
+    ],
+    "Turmas": [
+
     ]
 }
+
+def output_json(entidades):
+    return json.dumps(entidades, indent=4, ensure_ascii=False)
+
+aluno_test = Aluno(
+    100,
+    "Felipe Santos",
+    "2003/03/18", 
+    7.5, 
+    10.0, 
+    100,
+)
+
+professor_test = Professor(
+    100,
+    "João Augusto",
+    "1975/10/20",
+    "Desenvolvimento Web",
+    5000.0
+)
+
+turma_test = Turma(
+    100,
+    "ADS",
+    "Noite",
+    100
+)
+
+api_entidades["Alunos"].append(converter_aluno_dici(aluno_test.get()))
+api_entidades["Professores"].append(converter_professor_dici(professor_test.get()))
+api_entidades["Turmas"].append(converter_turma_dici(aluno_test.get()))
+
+print(output_json(api_entidades))
