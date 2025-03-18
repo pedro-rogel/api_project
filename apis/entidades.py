ca5cdb47924947f36f24e94936ce74a158c7433d
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-18 01:20:52 -0300

    style: apply code style fixes

--- apis/entidades.py
+++ apis/entidades.py
@@ -14,30 +14,30 @@ api_entidades = {
 }
 
 
-#Teste para verificar funcionalidade da atribuição utilizando objetos das classes de entidades
+# Teste para verificar funcionalidade da atribuição utilizando objetos das classes de entidades
 
 # aluno_test = Aluno(
-#     1,
-#     "Felipe Santos",
-#     "2004/04/19", 
-#     7.5, 
-#     10.0, 
-#     1,
+#     1,                                    # ID
+#     "Felipe Santos",                      # Nome
+#     "2004/04/19",                         # Data de Nascimento
+#     7.5,                                  # Nota 01 
+#     10.0,                                 # Nota 02
+#     1,                                    # ID da Turma
 # )
 
 # professor_test = Professor(
-#     1,
-#     "João Augusto",
-#     "1975/10/20",
-#     "Desenvolvimento Web",
-#     5000.0
+#     1,                                    # ID
+#     "João Augusto",                       # Nome
+#     "1975/10/20",                         # Data de Nascimento
+#     "Desenvolvimento Web",                # Disciplina
+#     5000.0                                # Salario
 # )
 
 # turma_test = Turma(
-#     1,
-#     "ADS",
-#     "Noite",
-#     1
+#     1,                                    # ID
+#     "ADS",                                # Nome
+#     "Noite",                              # Turno
+#     1                                     # ID Professor
 # )
 
 # api_entidades["alunos"].append(converter_aluno_dici(aluno_test.get()))
