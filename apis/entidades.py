f2c9b6ea4c5b49b08822933b2ad3e0c0ea3c1d18
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-18 21:16:44 -0300

    fix: bug in entidades.py

--- apis/entidades.py
+++ apis/entidades.py
@@ -1,5 +1,5 @@
-import json
-from classes_entidades import *
+# import json
+# from classes_entidades import *
 
 
 api_entidades = {
@@ -14,34 +14,34 @@ api_entidades = {
     ]
 }
 
-#Teste para verificar funcionalidade da atribuição utilizando objetos das classes de entidades
-def testar_atribuicao_entidades():
-    aluno_test = Aluno(
-        1,                                # ID
-        "Felipe Santos",                  # Nome
-        "2004/04/19",                     # Data de Nascimento
-        7.5,                              # Nota 01 
-        10.0,                             # Nota 02
-        1,                                # ID da Turma
-    )
-    
-    professor_test = Professor(
-        1,                                # ID
-        "João Augusto",                   # Nome
-        "1975/10/20",                     # Data de Nascimento
-        "Desenvolvimento Web",            # Disciplina
-        5000.0                            # Salario
-    )
 
-    turma_test = Turma(
-        1,                                # ID
-        "ADS",                            # Nome
-        "Noite",                          # Turno
-        1                                 # ID Professor
-    )
+# Teste para verificar funcionalidade da atribuição utilizando objetos das classes de entidades
+# aluno_test = Aluno(
+#     1,                                # ID
+#     "Felipe Santos",                  # Nome
+#     "2004/04/19",                     # Data de Nascimento
+#     7.5,                              # Nota 01 
+#     10.0,                             # Nota 02
+#     1,                                # ID da Turma
+# )
 
-    api_entidades["alunos"].append(aluno_test.converter_aluno_dici())
-    api_entidades["professores"].append(professor_test.converter_professor_dici())
-    api_entidades["turmas"].append(turma_test.converter_turma_dici())
+# professor_test = Professor(
+#     1,                                # ID
+#     "João Augusto",                   # Nome
+#     "1975/10/20",                     # Data de Nascimento
+#     "Desenvolvimento Web",            # Disciplina
+#     5000.0                            # Salario
+# )
 
-    return json.dumps(api_entidades, indent=4, ensure_ascii=False)
+# turma_test = Turma(
+#     1,                                # ID
+#     "ADS",                            # Nome
+#     "Noite",                          # Turno
+#     1                                 # ID Professor
+# )
+
+# api_entidades["alunos"].append(aluno_test.converter_aluno_dici())
+# api_entidades["professores"].append(professor_test.converter_professor_dici())
+# api_entidades["turmas"].append(turma_test.converter_turma_dici())
+
+# print(json.dumps(api_entidades, indent=4, ensure_ascii=False))
