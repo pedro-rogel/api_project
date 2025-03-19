9d621bef80db3b6b9dc341aa947282f692fea642
Pedro Rogel <pedrorogel3@gmail.com>
2025-03-19 19:56:17 -0300

    refactor: update condiditon of entity relationship in turmas.py

--- routes/turmas.py
+++ routes/turmas.py
@@ -5,6 +5,7 @@ from apis.classes_entidades import *
 turmas_bp = Blueprint("turmas", __name__)
 
 turmas = api_entidades["turmas"]
+professores = api_entidades["professores"]
 
 @turmas_bp.route("/turmas", methods=['GET'])
 def get_turmas():
@@ -16,6 +17,8 @@ def get_turmas_id(id):
         if turma["id"] == id:
             return jsonify(turma)
     return jsonify(erro="turma nao encontrada"), 400
+
+
 
 @turmas_bp.route("/turmas", methods=['POST'])
 def create_turma():
@@ -31,11 +34,8 @@ def create_turma():
     if not nova_turma.get("professor_id"):
         return jsonify(erro="turma sem professor"), 400
     else:
-        for professor in api_entidades['professores']:
-            if nova_turma.get("professor_id") == professor['id']:
-                nova_turma["professor_id"] = professor['id']
-            else:
-                return jsonify(erro="Id do professor não encontrado"), 400
+        if not any(professor["id"] == nova_turma["professor_id"] for professor in professores):
+            return jsonify(erro="Id do professor não encontrado"), 400
     if not any(turma["id"] == nova_turma["id"] for turma in turmas):
         obj_turma = Turma(nova_turma["id"], nova_turma['nome'], nova_turma['turno'], nova_turma['professor_id'])
         turmas.append(obj_turma.converter_turma_dici())
@@ -53,6 +53,8 @@ def update_turma(id):
             if atualizacao.get("turno"):
                 turma["turno"] = atualizacao["turno"]
             if atualizacao.get("professor_id"):
+                if not any(professor["id"] == atualizacao["professor_id"] for professor in professores):
+                    return jsonify(erro="Id do professor não encontrado"), 400
                 turma["professor_id"] = atualizacao["professor_id"]
             if atualizacao.get("id"):
                 turma['id'] = atualizacao['id']
