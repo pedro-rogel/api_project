c4f0e40c1615068f9ef37c5123027594909d9a9f
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-22 12:43:03 -0300

    chore: configure host IP and set port to 5000 in main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from flask import Flask, jsonify
-from routes.alunos import alunos_bp, alunos
 from routes.professores import professores_bp, professores
 from routes.turmas import turmas_bp, turmas
+from routes.alunos import alunos_bp, alunos
 
 app = Flask(__name__)
 
-app.register_blueprint(alunos_bp)
 app.register_blueprint(professores_bp)
 app.register_blueprint(turmas_bp)
+app.register_blueprint(alunos_bp)
 
 @app.route("/")
-def homepage():
+def index():
     return "Página raiz"
 
-@app.route("/reseta", methods=['POST'])
+@app.route("/resetar", methods=['POST'])
 def reset_server():
     alunos.clear()
-    professores.clear()
     turmas.clear()
+    professores.clear()
     return jsonify(message="resetado com sucesso")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=True, host='0.0.0.0', port=5000)
