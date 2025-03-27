dc3541f419e73e94b83c5c87555c9c33d5b20ba6
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-26 21:27:04 -0300

    refactor: changing alunos_routes and creating alunos_model

---
+++ routes/alunos_routes.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, jsonify, request
+from models.alunos_model import AlunoNaoEncontrado, listar_alunos, aluno_por_id, adicionar_aluno, atualizar_aluno, excluir_aluno
+
+alunos_bp = Blueprint("alunos", __name__)
+
+@alunos_bp.route("/alunos", methods=['GET'])
+def get_student():
+    return jsonify(listar_alunos())
+
+@alunos_bp.route("/alunos/<int:id>", methods=['GET'])
+def get_student_id(id):
+    try:
+        aluno = aluno_por_id(id)
+        return jsonify(aluno)
+    except AlunoNaoEncontrado:
+        return jsonify(erro="aluno nao encontrado"), 400
+
+@alunos_bp.route("/alunos", methods=['POST'])
+def create_student():
+    data = request.json
+    try:
+        adicionar_aluno(data)
+        return jsonify(data)
+    except AlunoNaoEncontrado:
+        return jsonify(erro="aluno nao encontrado"), 400
+
+@alunos_bp.route("/alunos/<int:id>", methods=['PUT'])
+def update_student(id):
+    data = request.json
+    try:
+        atualizar_aluno(id, data)
+        return jsonify(aluno_por_id(id))
+    except AlunoNaoEncontrado:
+        return jsonify(erro="aluno nao encontrado"), 400
+
+@alunos_bp.route("/alunos/<int:id>", methods=['DELETE'])
+def delete_student(id):
+    try:
+        excluir_aluno(id)
+        return jsonify(message="deletado com sucesso")
+    except AlunoNaoEncontrado:
+        return jsonify(erro="aluno nao encontrado"), 400
