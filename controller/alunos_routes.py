d947a23536594b4c8ebb5c9878ff6756deb0fbba
Pedro Rogel <pedrorogel3@gmail.com>
2025-04-07 12:34:45 -0300

    refactor: changing file name "routes" to "controller"

---
+++ controller/alunos_routes.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, jsonify, request
+from models.alunos_model import AlunoNaoEncontrado, listar_alunos, aluno_por_id, adicionar_aluno, atualizar_aluno, excluir_aluno
+
+alunos_bp = Blueprint("alunos", __name__)
+
+@alunos_bp.route("/alunos", methods=['GET'])
+def get_student():
+    return jsonify(listar_alunos()), 200
+
+@alunos_bp.route("/alunos/<int:id>", methods=['GET'])
+def get_student_id(id):
+    try:
+        return jsonify(aluno_por_id(id)), 200
+    except AlunoNaoEncontrado as erro:
+        return jsonify(erro=erro.msg), 400
+
+@alunos_bp.route("/alunos", methods=['POST'])
+def create_student():
+    data = request.json
+    try:
+        adicionar_aluno(data)
+        return jsonify(data), 200
+    except AlunoNaoEncontrado as erro:
+        return jsonify(erro=erro.msg), 400
+
+@alunos_bp.route("/alunos/<int:id>", methods=['PUT'])
+def update_student(id):
+    data = request.json
+    try:
+        atualizar_aluno(id, data)
+        return jsonify(aluno_por_id(id)), 200
+    except AlunoNaoEncontrado as erro:
+        return jsonify(erro=erro.msg), 400
+
+@alunos_bp.route("/alunos/<int:id>", methods=['DELETE'])
+def delete_student(id):
+    try:
+        excluir_aluno(id)
+        return jsonify(message="Deletado com sucesso"), 200
+    except AlunoNaoEncontrado as erro:
+        return jsonify(erro=erro.msg), 400
