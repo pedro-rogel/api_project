60270abff9e733c620b124eccf56b7338b34b389
Pedro Rogel <pedrorogel3@gmail.com>
2025-04-16 21:59:45 -0300

    feat: add flask-restx

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from flask import Flask
 from config import app
-from controller.alunos_routes import alunos_bp
-from controller.professores_routes import professores_bp
-from controller.turmas_routes import turmas_bp
+from swagger.config import api
+from swagger.school_doc import ns_api
+from controller.alunos_routes import alunos_bp  # ou o nome do seu arquivo real
+
+
 
 app.register_blueprint(alunos_bp)
-app.register_blueprint(professores_bp)
-app.register_blueprint(turmas_bp)
+api.init_app(app)
+api.add_namespace(ns_api)
 
 if __name__ == "__main__":
     app.run(host=app.config["HOST"], port=app.config["PORT"], debug=app.config["DEBUG"])
