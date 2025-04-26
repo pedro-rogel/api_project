1975fe3f62b26d78a994a0f1375d01b64da597ac
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-04-26 15:04:31 -0300

    refactor: adapt tdd.py for compatibility with app changes

--- tests/tdd.py
+++ tests/tdd.py
@@ -1,5 +1,5 @@
-import requests
 import unittest
+import requests
 
 class TestStringMethods(unittest.TestCase):
 
@@ -50,7 +50,6 @@ class TestStringMethods(unittest.TestCase):
 
     def test_007_alunos_post_sem_turmas(self):
         r = requests.post('http://localhost:9090/alunos',json={
-        "id": 1,
         "nome": "Pedro",
         "data_nascimento": "2006/06/01",
         "nota_primeiro_semestre": 5,
@@ -58,18 +57,19 @@ class TestStringMethods(unittest.TestCase):
         "turma_id": 1
         })
         self.assertIn(r.status_code,[400,404])
-        self.assertEqual(r.json()['erro'],'Não há turmas para criar Alunos')
+        self.assertEqual(r.json()['erro'],'Nao ha turmas para criar alunos')
 
 
     def test_008_turmas_post_sem_professores(self):
-        r = requests.post('http://localhost:9090/turmas',json={
-            'id':1, 
+        r = requests.post('http://localhost:9090/turmas',json={ 
             'nome':'ADS', 
-            'turno':'Diurno', 
+            'turno':'Diurno',
+            'descricao': 'Teste',
+            'status': True,
             'professor_id': 1
             })
         self.assertIn(r.status_code,[400,404])
-        self.assertEqual(r.json(), {'error': 'Não há professores'})
+        self.assertEqual(r.json(), {'erro': 'Nao ha professores para criar turmas'})
 
 
     def test_009_professores_post_criar_professor(self):
@@ -140,41 +140,41 @@ class TestStringMethods(unittest.TestCase):
 
     def test_012_alunos_get_id(self):
         r_lista = requests.get('http://localhost:9090/alunos/2')
-        self.assertEqual(r_lista.json()['nome'],'Fernando Barril')
+        self.assertEqual(r_lista.json()['data']['nome'],'Fernando Barril')
 
 
     def test_013_professores_get_id(self):
         r_lista = requests.get('http://localhost:9090/professores/2')
-        self.assertEqual(r_lista.json()['nome'],'Marcelo')
+        self.assertEqual(r_lista.json()['data']['nome'],'Marcelo')
 
 
     def test_014_turmas_get_id(self):
         r_lista = requests.get('http://localhost:9090/turmas/2')
-        self.assertEqual(r_lista.json()['nome'],'SI')
+        self.assertEqual(r_lista.json()['data']['nome'],'SI')
 
 
     def test_015_alunos_put_edita(self):
         r = requests.get('http://localhost:9090/alunos/2')
-        self.assertEqual(r.json()['nome'],'Fernando Barril')
+        self.assertEqual(r.json()['data']['nome'],'Fernando Barril')
         requests.put('http://localhost:9090/alunos/2', json={'nome':'Felippe Fodase'})
         r_depois = requests.get('http://localhost:9090/alunos/2')
-        self.assertEqual(r_depois.json()['nome'],'Felippe Fodase')
+        self.assertEqual(r_depois.json()['data']['nome'],'Felippe Fodase')
 
 
     def test_016_professores_put_edita(self):
         r = requests.get('http://localhost:9090/professores/1')
-        self.assertEqual(r.json()['nome'],'João')
+        self.assertEqual(r.json()['data']['nome'],'João')
         requests.put('http://localhost:9090/professores/1', json={'nome':'Gustavo Santos'})
         r_depois = requests.get('http://localhost:9090/professores/1')
-        self.assertEqual(r_depois.json()['nome'],'Gustavo Santos')
+        self.assertEqual(r_depois.json()['data']['nome'],'Gustavo Santos')
 
 
     def test_017_turmas_put_edita(self):
         r = requests.get('http://localhost:9090/turmas/1')
-        self.assertEqual(r.json()['nome'],'ADS')
+        self.assertEqual(r.json()['data']['nome'],'ADS')
         requests.put('http://localhost:9090/turmas/1', json={'nome':'ADS 2B'})
         r_depois = requests.get('http://localhost:9090/turmas/1')
-        self.assertEqual(r_depois.json()['nome'],'ADS 2B')
+        self.assertEqual(r_depois.json()['data']['nome'],'ADS 2B')
 
 
     def test_018_alunos_delete(self):
