8ef83f0467d3e1809b44aa69a5608de39bfdcb20
pedro rogel <pedro.rogel@articocapital.com.br>
2025-04-28 14:56:52 -0300

    fix: fix swagger models and ProfessoresGet list to dict

--- app/swagger/professores_doc.py
+++ app/swagger/professores_doc.py
@@ -24,16 +24,17 @@ model_professores_output = ns_api_professores.model("ProfessoresOutPut", {
 
 @ns_api_professores.route('/')
 class ProfessoresGetPost(Resource):
-    @ns_api_professores.response(200,"Código", model_professores)
+    @ns_api_professores.response(200,"Código", model_professores_output)
     def get(self):
+        """Retorna todos os professores cadastrados"""
         try:
-            return {listar_professores()}, 200
+            return {"data" : listar_professores()}, 200
         except ProfessorException as erro:
             return {"erro": erro.msg}, 400
     
     @ns_api_professores.expect(model_professores)
-    @ns_api_professores.doc(description="Cria um novo professor")  
     def post(self):
+        """Cria um novo professor"""
         data = api.payload
         try:
             msg = adicionar_professor(data)
@@ -41,21 +42,20 @@ class ProfessoresGetPost(Resource):
         except ProfessorException as erro:
             return {"erro": erro.msg}, 400
         
-@ns_api_professores.expect(model_professores_output)
 @ns_api_professores.route('/<int:id>')
 @ns_api_professores.param('id', 'Id do Professor')
 class ProfessorPorId(Resource):
-    @ns_api_professores.response(200, "Professores CRUD", model_professores)
-    @ns_api_professores.doc(description="Retorna o professor pelo id")
+    @ns_api_professores.marshal_list_with(model_professores_output)
     def get(self, id):
+        """Retorna um professor pelo id passado"""
         try:
             return {"data": professor_por_id(id)}, 200
         except ProfessorException as erro:
             return {"erro": erro.msg}, 400  
     
     @ns_api_professores.expect(model_professores)
-    @ns_api_professores.doc(description="Atualiza os dados do professor pelo id")
     def put(self, id):
+        """Atualiza o professor pelo id passado"""
         data = api.payload
         try: 
             msg = atualizar_professor(id, data)
@@ -63,8 +63,9 @@ class ProfessorPorId(Resource):
         except ProfessorException as erro:
             return {"erro": erro.msg}, 400
     
-    @ns_api_professores.doc(description="Deleta o professor pelo id")    
+
     def delete(self, id):
+        """Deleta o professor pelo id passado"""
         try:
             msg = deletar_professor(id)
             return {"data": msg}, 200
